3698ae30149d9ef3d39db65f288afef6c2592998
devonbriscoe-gif <devon.briscoe@sarahtreed.org>
2026-09-17 18:01:21 +0000

    replace me !

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,38 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
-    // TODO 2 - Create Platforms
+
+    // TODO 2 - CreatePlatform 
+createPlatform(100, 20, 10, 10, "lime"); 
+createPlatform(500, 0, 20, 290);
+createPlatform(1350, 400, 50, 50, "red");
+
+
+
+
 
 
 
 
     // TODO 3 - Create Collectables
+    createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
+
+    
+
 
 
 
     
     // TODO 4 - Create Cannons
+    createCannon("top", 200, 100);
+createCannon("right", 300, 2000);
+
+  
+
 
 
     
